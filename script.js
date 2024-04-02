7cbf3aea17f9b4b2594abdbed1cd818475be7f64
Nodirjonov <sobirovakomila85@gmail.com>
2024-04-03 02:12:40 +0500

    5oy 5js

---
+++ script.js
@@ -0,0 +1,17 @@
+// let a = document.querySelector('h1');
+// a.classList.add('box')
+// a.classList.remove('example')
+// console.log(a.classList);
+
+var name = prompt("Введите первое слово");
+var F = document.getElementById("FFF");
+F.textContent = name ;
+
+var ID = prompt("Введите второе");
+var A = document.getElementById("AAA");
+A.textContent = ID ;
+
+var group = prompt("Введите третье слово");
+var S = document.getElementById("SSS");
+S.textContent = group ;
+
